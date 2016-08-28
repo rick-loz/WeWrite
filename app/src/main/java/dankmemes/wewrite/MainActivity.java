2c3b38cb3883659e9925df2f398698fbf7586387
rick-loz <rickyme98@hotmail.com>
2016-08-28 05:45:56 -0500

    Modified RoomSelection.java

--- app/src/main/java/dankmemes/wewrite/MainActivity.java
+++ app/src/main/java/dankmemes/wewrite/MainActivity.java
@@ -2,12 +2,32 @@ package dankmemes.wewrite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference roomLobby;
+    int iContador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+
+
+        roomLobby = database.getReference("RoomLobby");
+
+        if(iContador == 0)
+        {
+           DatabaseReference room = roomLobby.child("Room");
+        }
+
     }
 }
